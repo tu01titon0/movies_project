7175b640a04af515f4eecd7653c98d5acc74f2d6
tu <nguyenmanhtu09042000@gmail.com>
2023-05-29 14:28:11 +0700

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const http = require('http');
 const url = require('url');
 const qs = require('qs');
-const mysql = require('mysql');
-
 const PORT = 3333;
-
 
 const app = http.createServer((req, res) => {
     const pathUrl = url.parse(req.url).pathname;
