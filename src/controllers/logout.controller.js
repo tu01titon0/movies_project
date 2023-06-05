12ad1123cc13c9dfefc819b4ac57e72286eaa795
KY <nguyenhoangky23121997@gmail.com>
2023-06-05 14:59:32 +0700

    first

--- src/controllers/logout.controller.js
+++ src/controllers/logout.controller.js
@@ -5,9 +5,9 @@ class LogoutController {
     async logout(req, res) {
         let id = req.user.id
         await BaseController.deleteFileData('./session/user-' + id)
+        res.setHeader('Cache-Control','no-cache, no-store, must-revalidate')
         res.writeHead(301, {Location: '/login'});
         return res.end()
     }
 }
-
 module.exports = new LogoutController
